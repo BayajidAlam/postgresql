8a5862c10a443d962e99c152291f56ad00502e24
BayajidAlam <bayajidalam2001@gmail.com>
2023-08-17 16:45:03 +0600

    inser,update,delete

--- command.sql
+++ command.sql
@@ -85,3 +85,81 @@ DROP constraint unique_email
 
 -- get all row of a table  
 select * from "user";
+
+
+
+
+
+
+-- Foreign Key Constraint And Data Integrity
+-- Employee Table
+-- Each employee  belongs to a department
+
+-- create table
+CREATE TABLE employee (
+  empId SERIAL PRIMARY KEY,
+  empName VARCHAR(50) NOT NULL,
+  departmentId INT,
+  CONSTRAINT fk_constraint_dept
+    FOREIGN KEY(departmentId)
+    REFERENCES Department(deptID)
+);
+
+-- insert into employee table
+INSERT INTO employee VALUES(1,'Bayajid',1);
+
+-- Department TABLE
+-- Each department has many employee 
+-- create table
+CREATE TABLE Department(
+  deptID SERIAL PRIMARY KEY,
+  deptName VARCHAR(50)
+);
+
+-- insert into Department table
+INSERT INTO Department VALUES(1,'IT')
+
+-- see table
+SELECT * from Department
+SELECT * from employee
+
+
+
+
+
+-- Updation And Deletion 
+-- create courses table
+CREATE Table courses (
+  course_id SERIAL PRIMARY KEY,
+  course_name VARCHAR(255) not NULL,
+  description VARCHAR(255),
+  published_data DATE
+);
+-- Insert into courses table
+INSERT INTO courses (course_name, description, published_data)
+VALUES
+  ('Course 1', 'Description for Course 1', '2023-08-17'),
+  ('Course 2', 'Description for Course 2', NULL),
+  ('Course 3', NULL,NULL),
+  ('Course 4', 'Description for Course 4', '2023-08-20'),
+  ('Course 5', 'Description for Course 5', '2023-08-21');
+
+-- update table row 
+-- UPDATE table_name
+-- set 
+-- column1 = value1 
+-- COLUMN2 = value2 
+-- WHERE CONDITION
+
+UPDATE courses
+set
+course_name = 'Postgres2',
+"description" = 'Dummy Text2'
+WHERE course_id = 1 OR course_id = 5
+
+
+-- DELETE 
+DELETE from courses
+WHERE course_id = 5;
+
+SELECT * from courses;
