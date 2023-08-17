cbd4bdb584344199b001e8bb1dbd126a19fc608c
BayajidAlam <bayajidalam2001@gmail.com>
2023-08-17 22:25:57 +0600

    assignment solution added

--- command.sql
+++ command.sql
@@ -349,3 +349,155 @@ SELECT deptid,AVG(salary) from employees2 GROUP BY deptid;
 -- SELECT d.name,SUM(e.salary), min(e.salary), count(*) FROM employees2 e
 -- FULL JOIN employees2 e on e.deptId = d.dept
 -- GROUP BY d.deptId
+
+
+
+
+
+-- Assignment 
+-- Active: 1692246012307@@127.0.0.1@5432
+
+-- create database 
+CREATE DATABASE university_db;
+
+-- create students table 
+CREATE TABLE students (
+  student_id SERIAL PRIMARY KEY,
+  student_name VARCHAR(255) NOT NULL,
+  age INTEGER,
+  email VARCHAR(255) UNIQUE,
+  frontend_mark INTEGER,
+  backend_mark INTEGER,
+  status VARCHAR(50)
+);
+
+-- sample data 
+INSERT INTO students (student_id, student_name, age, email, frontend_mark, backend_mark, status)
+VALUES
+  (1, 'Alice', 22, 'alice@example.com', 55, 57, NULL),
+  (2, 'Bob', 21, 'bob@example.com', 34, 45, NULL),
+  (3, 'Charlie', 23, 'charlie@example.com', 60, 59, NULL),
+  (4, 'David', 20, 'david@example.com', 40, 49, NULL),
+  (5, 'Eve', 24, 'newemail@example.com', 45, 34, NULL),
+  (6, 'Rahim', 23, 'rahim@gmail.com', 46, 42, NULL);
+
+-- create courses table 
+CREATE TABLE courses (
+  course_id SERIAL PRIMARY KEY,
+  course_name VARCHAR(255) NOT NULL,
+  credits INTEGER
+);
+
+-- sample data 
+INSERT INTO courses (course_id, course_name, credits)
+VALUES
+  (1, 'Next.js', 3),
+  (2, 'React.js', 4),
+  (3, 'Databases', 3),
+  (4, 'Prisma', 3);
+
+-- create enrollment 
+CREATE TABLE enrollment (
+  enrollment_id SERIAL PRIMARY KEY,
+  student_id INTEGER,
+  course_id INTEGER,
+  FOREIGN KEY (student_id) REFERENCES students(student_id),
+  FOREIGN KEY (course_id) REFERENCES courses(course_id)
+);
+
+-- sample data 
+INSERT INTO enrollment (enrollment_id, student_id, course_id)
+VALUES
+  (1, 1, 1),
+  (2, 1, 2),
+  (3, 2, 1),
+  (4, 3, 2);
+
+SELECT * from students;
+SELECT * from courses;
+SELECT * from enrollment;
+
+
+-- QUERY 1 
+-- Insert a new student record with the following details:
+-- Name: YourName
+-- Age: YourAge
+-- Email: YourEmail
+-- Frontend-Mark: YourMark
+-- Backend-Mark: YourMark
+-- Status: NULL
+INSERT INTO students VALUES (
+  7, 'Bayajid', 22, 'bayzedalam2001@gmail.com', 55, 57, NULL
+  );
+
+  -- QUERY2
+-- Retrieve the names of all students who are enrolled in the course titled 'Next.js'.
+-- Sample Output:
+-- student_name
+-- Alice
+-- Bob
+SELECT s.student_name
+FROM students s
+JOIN enrollment e ON s.student_id = e.student_id
+JOIN courses c ON e.course_id = c.course_id
+WHERE c.course_name = 'Next.js';
+
+  -- QUERY3
+  -- Update the status of the student with the highest total (frontend_mark + backend_mark) mark to 'Awarded' 
+UPDATE students
+SET status = 'Awarded'
+WHERE (frontend_mark + backend_mark) = (
+  SELECT MAX(frontend_mark + backend_mark)
+  FROM students
+);
+
+  -- QUERY4
+  -- Delete all courses that have no students enrolled.
+DELETE FROM courses
+WHERE course_id NOT IN (
+  SELECT DISTINCT course_id
+  FROM enrollment
+);
+
+  -- QUERY5
+-- Retrieve the names of students using a limit of 2, starting from the 3rd student.
+-- Sample Output:
+-- student_name
+-- Charlie
+-- David
+SELECT student_name
+FROM students
+OFFSET 2
+LIMIT 2;
+
+  -- QUERY6
+--   Retrieve the course names and the number of students enrolled in each course.
+-- Sample Output:
+-- course_name	students_enrolled
+-- Next.js	2
+-- React.js	2
+SELECT c.course_name, COUNT(e.student_id) AS enrolled_students
+FROM courses c
+LEFT JOIN enrollment e ON c.course_id = e.course_id
+GROUP BY c.course_name;
+
+  -- QUERY7
+--   Calculate and display the average age of all students.
+-- Sample Output:
+-- average_age
+-- 22.2857142857142857
+  average of the all students
+SELECT AVG(age) AS average_age
+FROM students;
+
+  -- QUERY8
+-- Retrieve the names of students whose email addresses contain 'example.com'.
+-- Sample Output:
+-- student_name
+-- Alice
+-- Bob
+-- Charlie
+-- David
+SELECT student_name
+FROM students
+WHERE email LIKE '%example.com%';
