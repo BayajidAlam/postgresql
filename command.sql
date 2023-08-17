fd3f2891bd64156fd1ade659f642712231b8e62c
BayajidAlam <bayajidalam2001@gmail.com>
2023-08-18 01:14:54 +0600

    more solutions added

--- command.sql
+++ command.sql
@@ -341,14 +341,6 @@ SELECT max(salary) averageSalary FROM employees2;
 SELECT deptid,AVG(salary) from employees2 GROUP BY deptid;
 
 
--- for example(with some inappropieate)
--- SELECT d.name,AVG(e.salary), max(e.salary) FROM employees2 e
--- FULL JOIN department2 d on e.deptId = d.dept
--- GROUP BY d.name Having avg(e.salary) > 52000
-
--- SELECT d.name,SUM(e.salary), min(e.salary), count(*) FROM employees2 e
--- FULL JOIN employees2 e on e.deptId = d.dept
--- GROUP BY d.deptId
 
 
 
@@ -486,7 +478,7 @@ GROUP BY c.course_name;
 -- Sample Output:
 -- average_age
 -- 22.2857142857142857
-  average of the all students
+  -- average of the all students
 SELECT AVG(age) AS average_age
 FROM students;
 
@@ -501,3 +493,177 @@ FROM students;
 SELECT student_name
 FROM students
 WHERE email LIKE '%example.com%';
+
+
+
+
+
+
+
+-- practice task 
+-- Query 1
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    department VARCHAR(50),
+    salary DECIMAL(10, 2)
+);
+
+INSERT INTO employees (emp_id, emp_name, department, salary)
+VALUES
+    (1, 'John Doe', 'HR', 50000.00),
+    (2, 'Jane Smith', 'IT', 60000.00),
+    (3, 'Michael Johnson', 'Finance', 55000.00),
+    (4, 'Emily Brown', 'HR', 52000.00);
+
+-- Write an SQL query to retrieve the names and salaries of employees who work in the "HR" department.
+SELECT emp_name,salary from employees
+WHERE department = 'HR';
+
+-- Query 2
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    total_amount DECIMAL(10, 2)
+);
+
+INSERT INTO orders (order_id, customer_id, total_amount)
+VALUES
+    (101, 1, 200.00),
+    (102, 2, 300.00),
+    (103, 1, 150.00),
+    (104, 3, 400.00),
+    (105, 2, 250.00);
+
+-- Write an SQL query to find the customer IDs and the average total amount of their orders. Display only those customers whose average total amount is greater than or equal to 250.
+SELECT customer_id, AVG(total_amount) as average_total_amount 
+FROM orders 
+GROUP BY customer_id 
+HAVING avg(total_amount) >= 250;
+
+-- Query 3
+CREATE TABLE students (
+    student_id INT PRIMARY KEY,
+    student_name VARCHAR(50),
+    age INT,
+    gender VARCHAR(10)
+);
+
+CREATE TABLE courses (
+    course_id INT PRIMARY KEY,
+    course_name VARCHAR(50),
+    credits INT
+);
+
+INSERT INTO students (student_id, student_name, age, gender)
+VALUES
+    (1, 'Alice', 22, 'Female'),
+    (2, 'Bob', 21, 'Male'),
+    (3, 'Charlie', 23, 'Male');
+
+INSERT INTO courses (course_id, course_name, credits)
+VALUES
+    (101, 'Mathematics', 3),
+    (102, 'History', 4),
+    (103, 'Physics', 3);
+
+-- Enrollment table with student-course relationships
+CREATE TABLE enrollment (
+    enrollment_id INT PRIMARY KEY,
+    student_id INT,
+    course_id INT
+);
+
+INSERT INTO enrollment (enrollment_id, student_id, course_id)
+VALUES
+    (1, 1, 101),
+    (2, 1, 102),
+    (3, 2, 103),
+    (4, 3, 101);
+
+
+-- Write an SQL query to retrieve the student name, course name, and credits for all enrolled courses.
+SELECT s.student_name,cr.course_name,cr.credits 
+from students s
+JOIN enrollment e on e.student_id = s.student_id
+JOIN courses cr on cr.course_id = e.course_id
+
+-- Query 4
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    department_id INT
+);
+
+CREATE TABLE departments (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(50)
+);
+
+CREATE TABLE salaries (
+    emp_id INT,
+    salary DECIMAL(10, 2)
+);
+
+INSERT INTO employees (emp_id, emp_name, department_id)
+VALUES
+    (1, 'John Doe', 1),
+    (2, 'Jane Smith', 2),
+    (3, 'Michael Johnson', 1),
+    (4, 'Emily Brown', 3);
+
+INSERT INTO departments (department_id, department_name)
+VALUES
+    (1, 'HR'),
+    (2, 'IT'),
+    (3, 'Finance');
+
+INSERT INTO salaries (emp_id, salary)
+VALUES
+    (1, 50000.00),
+    (2, 60000.00),
+    (3, 55000.00),
+    (4, 52000.00);
+
+--  Write an SQL query to retrieve the department name and the average salary of employees working in each department. Sort the results by the average salary in descending order.
+SELECT d.department_name, AVG(s.salary) as average_salary
+from employees e
+JOIN departments d on d.department_id= e.department_id
+JOIN salaries s on s.emp_id = e.emp_id
+GROUP BY d.department_name 
+ORDER BY avg(salary) DESC
+
+
+-- Query 5 
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    total_amount DECIMAL(10, 2)
+);
+
+SELECT 
+    TO_CHAR(order_date, 'YYYY-MM') AS month,
+    COUNT(order_id) AS number_of_orders,
+    SUM(total_amount) AS total_sales_amount
+FROM orders
+GROUP BY TO_CHAR(order_date, 'YYYY-MM')
+ORDER BY TO_CHAR(order_date, 'YYYY-MM');
+
+
+-- subquery 
+SELECT * from employees WHERE salary IN (
+    SELECT salary from employees WHERE name LIKE '%a%'
+)
+
+SELECT * from employees WHERE salary  > (
+    SELECT avg(salary) from employees
+)
+
+SELECT email,(
+    SELECT avg(salary) from employees
+) FROM employees
+
+SELECT dept_id, avg_salary from (
+    SELECT deptId as dept_id,avg(salary) as avg_salary  from employees
+) as tempTable
